325659bdb797cd4473e3e4ade436d7b09c25733d
zyx1104952466 <1104952466@qq.com>
2022-02-24 10:12:10 +0800

    提交

---
+++ yuxiang-file/src/main/java/com/yuxiang/FileApplication.java
@@ -0,0 +1,17 @@
+package com.yuxiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyux
+ * @version 1.0
+ * @date 2022/2/23 19:37
+ */
+@SpringBootApplication
+public class FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
---
+++ yuxiang-file/src/main/java/com/yuxiang/startup/MyStartupRunner.java
@@ -0,0 +1,27 @@
+package com.yuxiang.startup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName MyStartupRunner
+ * @Description TODO
+ * @Author zhangyux
+ * @Date 2022/02/23 17:04
+ * @Version 1.0
+ **/
+@Component
+@Order(value = 1)//代表启动时加载的顺序
+public class MyStartupRunner implements CommandLineRunner {
+
+    public static final Logger logger = LoggerFactory.getLogger(MyStartupRunner.class);
+
+    @Override
+    public void run(String... args) {
+        logger.info("MyStartupRunner");
+    }
+}
