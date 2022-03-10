647eed3b69c6e05a5c48c64ff446dc60cd787179
zyx1104952466 <1104952466@qq.com>
2022-03-10 19:50:11 +0800

    提交代码(调整目录)

--- yuxiang-admin/yuxiang-admin-manager/src/main/java/com/yuxiang/manager/config/dubbo/DubboReferenceLoad.java
+++ yuxiang-admin/yuxiang-admin-manager/src/main/java/com/yuxiang/manager/config/dubbo/DubboReferenceLoad.java
@@ -1,6 +1,7 @@
 package com.yuxiang.manager.config.dubbo;
 
 import com.yuxiang.remote.iface.IBatchTaskCenter;
+import com.yuxiang.remote.iface.search.IBatchTaskCenterSearch;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,6 @@ import org.springframework.context.annotation.Configuration;
 public class DubboReferenceLoad {
 
     @DubboReference(version = "1.0.0", protocol = "dubbo", timeout = 30000, check = false)
-    IBatchTaskCenter batchTaskCenter;
+    IBatchTaskCenterSearch batchTaskCenterSearch;
 
 }
--- yuxiang-admin/yuxiang-admin-manager/src/main/java/com/yuxiang/manager/controller/BatchTaskCenterController.java
+++ yuxiang-admin/yuxiang-admin-manager/src/main/java/com/yuxiang/manager/controller/BatchTaskCenterController.java
@@ -4,7 +4,7 @@ import com.yuxiang.comm.ListResult;
 import com.yuxiang.dto.BatchTaskCenterDto;
 import com.yuxiang.page.Pager;
 import com.yuxiang.page.Paginater;
-import com.yuxiang.remote.iface.IBatchTaskCenter;
+import com.yuxiang.remote.iface.search.IBatchTaskCenterSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BatchTaskCenterController {
 
     @Autowired
-    IBatchTaskCenter batchTaskCenterServer;
+    IBatchTaskCenterSearch batchTaskCenterSearch;
 
     @RequestMapping("ajaxData")
     public Object findPage(int page, int rows, BatchTaskCenterDto dto) {
         Pager pager = new Pager();
         pager.setPageSize(rows);
         pager.setPageNumber(page);
-        Paginater paginater = batchTaskCenterServer.findPager(dto, pager);
+        Paginater paginater = batchTaskCenterSearch.findPager(dto, pager);
         return new ListResult(paginater.getData(), paginater.getMaxRowCount());
     }
 }
