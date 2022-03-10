559956532e3b0788e267d0a4a7eb9bd6262ba4bf
zyx1104952466 <1104952466@qq.com>
2022-03-10 11:18:14 +0800

    提交代码(添加UI模块)

---
+++ yuxiang-public/src/main/java/com/yuxiang/comm/ListResult.java
@@ -0,0 +1,40 @@
+package com.yuxiang.comm;
+
+/**
+ * @Author: cw
+ * @Date: 2021-11-4 10:39
+ * @ClassName: ListResult
+ * @Description: 列表结果
+ */
+public class ListResult<T> extends Result<T> {
+    private T rows;
+    private Long total;
+
+    public ListResult() {
+    }
+
+    public ListResult(T rows, Long total) {
+        this.setTotal(total);
+        this.setRespCode(ReturnCodeEnum.SUCCESS.getCode());
+        this.setRespDesc(ReturnCodeEnum.SUCCESS.getDesc());
+        this.setRespData(rows);
+    }
+
+    @Override
+    public void setRespData(T respData) {
+        super.setRespData(respData);
+        this.rows = respData;
+    }
+
+    public T getRows() {
+        return rows;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
